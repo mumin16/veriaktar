13c634da80b07122a36b8092701c9703125bd37f
mumin guler <mumin16@hotmail.com>
2024-11-02 16:24:11 +0300

    Add files via upload

---
+++ resource.h
@@ -0,0 +1,76 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+//
+#define IDD_DIALOG1                     101
+#define IDR_MENU1                       103
+#define IDI_ICON1                       105
+#define IDC_BILGI                       1001
+#define IDC_SYMBOL1                     1003
+#define IDC_SYMBOLS                     1003
+#define IDC_COMBO1                      1004
+#define IDC_EKLE                        1005
+#define IDC_SYMBOLADD                   1005
+#define IDC_HEPSINIEKLE                 1006
+#define IDC_CIKAR                       1007
+#define IDC_SYMBOLREMOVE                1007
+#define IDC_HEPSINICIKAR                1008
+#define IDC_SYMBOLSCLEAR                1008
+#define IDC_SYMBOL2                     1009
+#define IDC_GUNLUK                      1010
+#define IDC_DAILY                       1010
+#define IDC_1DAKIKA                     1011
+#define IDC_INTRADAY                    1011
+#define IDC_EDIT1                       1020
+#define IDC_SYMBOL                      1020
+#define IDC_XDAKIKA                     1021
+#define IDC_XMINUTE                     1021
+#define IDC_PERIOD                      1021
+#define IDC_CHECK1                      1022
+#define IDC_TEKRAR                      1022
+#define IDC_EXTENSION                   1022
+#define IDC_EDIT2                       1023
+#define IDC_OUTPUTDIR                   1023
+#define IDC_BUTTON1                     1024
+#define IDC_MSDIRSELECT                 1024
+#define IDC_STATICINFO                  1030
+#define IDC_EDIT3                       1031
+#define IDC_TARGETDIR                   1031
+#define IDC_TARGETDIRSELECT             1032
+#define IDC_EDIT4                       1033
+#define IDC_DIVISOR                     1033
+#define IDC_ACTION                      1034
+#define IDC_EDIT5                       1035
+#define IDC_LENGTH                      1035
+#define IDC_CHECK2                      1036
+#define IDC_LOOP                        1036
+#define IDR_HAKKINDA                    40003
+#define IDR_VERIAKTAR                   40004
+#define IDR_PORTFOYYUKLE                40005
+#define IDR_PORTFOYKAYDET               40006
+#define IDR_AYARLAR                     40014
+#define IDR_METATRADERAKTAR             40016
+#define IDR_MT4                         40017
+#define IDR_DIGER                       40018
+#define ID_DIGER_HAKKINDA               40019
+#define ID_HAKKINDA                     40020
+#define ID_DIGER_GUNCELLEMEKONTROL      40021
+#define IDR_GUNCELLEMEKONTROL           40022
+#define ID_DIGER_SEMBOLLERIGUNCELLE     40023
+#define IDR_SEMBOLLERIGUNCELLE          40024
+#define ID_METATRADER4_MT4              40025
+#define ID_METATRADER4_MT5              40026
+#define IDR_MT5                         40027
+#define ID_HAKKINDA40028                40028
+#define IDR_ABOUT                       40029
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        111
+#define _APS_NEXT_COMMAND_VALUE         40030
+#define _APS_NEXT_CONTROL_VALUE         1037
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
